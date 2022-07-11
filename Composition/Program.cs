7c6f190cc43246e234e1d1878c1c4376ec7ab6d3
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-11 09:17:53 +0200

    all in one file

--- Composition/Program.cs
+++ Composition/Program.cs
@@ -5,8 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 //https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2243556#content
+
+// Composition is a relationship between two classes that allows one to contain
+// the other
+
+//  Inheritance:            | Composition:
+//* is-a relationship       | has-a relationship
+//* square is a rectangle   | car has an engine 
 namespace Composition
 {
+    public class DbMigrator
+    {
+    private readonly Logger logger;
+
+    public DbMigrator(Logger logger)
+    {
+        this.logger = logger;
+    }
+    public void Migrate()
+    {
+        logger.Log("We are migrating sdsdasd");
+    }
+}
+public class Installer
+{
+    private readonly Logger logger;
+
+    public Installer(Logger logger)
+    {
+        this.logger = logger;
+    }
+    public void Install()
+    {
+        logger.Log("We are installing the application.");
+    }
+}
+public class Logger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+
     class Program
     {
         static void Main(string[] args)
